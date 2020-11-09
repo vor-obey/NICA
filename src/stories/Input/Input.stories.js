300786f436620f89f640d9d83ec67013a621c8f0
Victor Vorobiov <vv1641627@gmail.com>
2020-11-09 17:35:13 +0200

    created the registration form component

---
+++ src/stories/Input/Input.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Input} from "./Input";
+
+export default {
+    title: 'Input',
+    component: Input
+}
+
+export const Template = (args) => <Input name='YOUR_LABEL' {...args} />
+
