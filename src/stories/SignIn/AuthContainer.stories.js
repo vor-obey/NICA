57914f9ade1fb70cf82eda8d03dbb9526509425f
Victor Vorobiov <vv1641627@gmail.com>
2020-11-10 17:05:53 +0200

    delete stories

--- src/stories/SignIn/AuthContainer.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {AuthContainer} from "./AuthContainer";
-
-export default {
-    title: 'SignInBlock',
-    component: AuthContainer
-}
-
-export const Template = () => <AuthContainer children={'YOUR_CONTENT'} nameBtn='Button'/>
