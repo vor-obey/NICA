cd4862544a6ae3e23660c7011ae324ae288dc5cf
Victor Vorobiov <vv1641627@gmail.com>
2020-11-11 12:00:51 +0200

    Create CenterDecorator for stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import {addDecorator} from '@storybook/react';
+import Center from '../src/components/CenterDecorator/CenterDecorator';
+
+addDecorator(story => <Center>{story()}</Center>)
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
