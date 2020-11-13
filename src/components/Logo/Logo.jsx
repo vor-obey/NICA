508c904a25164e6fbd88408b5f72402dd1c8c9e5
Artur Chubukliiev <artur.it2914@gmail.com>
2020-11-13 11:14:14 +0200

    Rename NicaLogo to Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import LogoIcon from './NICA_lockup.svg';
 import styles from './Logo.module.scss';
 
 const Logo = (props) => (
-  <img src={LogoIcon} alt="NICA" {...props} className={styles.logo} />
+  <img src={LogoIcon} alt="NICA" className={styles.logo} {...props} />
 );
 
 export default Logo;
