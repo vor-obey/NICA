b04fdf13a0916d6d6073cbde4e117170f7680225
Victor Vorobiov <vv1641627@gmail.com>
2020-11-16 17:22:49 +0200

    Refactor OrderHistory

--- src/layouts/Dashboard/components/OrderHistory/OrderHistory.jsx
+++ src/layouts/Dashboard/components/OrderHistory/OrderHistory.jsx
@@ -10,13 +10,7 @@ import styles from '../../containers/Index/Index.module.scss';
 const { Text } = Typography;
 const { Panel } = Collapse;
 
-const paidData = [
-  'Paid $110.00 with Check on 2016-09-22 12:35:29 EDT',
-  'Paid $400.00 with Check on 2016-09-22 12:55:29 EDT',
-  'Paid $130.00 with Check on 2016-09-22 12:45:29 EDT',
-];
-
-const OrderHistory = ({ columns, orders }) => {
+const OrderHistory = ({ columns, orders, name }) => {
   const renderOrders = orders.map((order) => (
     <div key={order.id} style={{ marginBottom: 80 }}>
       <Divider orientation="left" style={{ color: '#1890ff' }}>
@@ -24,12 +18,12 @@ const OrderHistory = ({ columns, orders }) => {
       </Divider>
       <Row style={{ padding: 20 }}>
         <Text>
-          {`Placed on ${order.date} EDT by Kenny Griffin`}
+          {`Placed on ${order.date} EDT by ${name}`}
         </Text>
       </Row>
       <Table
         columns={columns}
-        dataSource={orders}
+        dataSource={order.products}
         pagination={false}
       />
       <Row className={styles.paragraphStyles}>
@@ -38,20 +32,20 @@ const OrderHistory = ({ columns, orders }) => {
         </Col>
         <Col span={22.5}>
           <List
-            header="Order total: $640.00"
-            dataSource={paidData}
+            header={`Order total: $${order.total} `}
+            dataSource={order.products}
             renderItem={(item) => (
               <List.Item>
                 <Typography.Text>
                   <CaretRightOutlined style={{ marginRight: 10 }} />
                 </Typography.Text>
-                {item}
+                {`Paid ${item.price} with Check on ${order.date}`}
               </List.Item>
             )}
           />
         </Col>
       </Row>
-      {order.paid
+      {order.status === 'paid'
         ? (
           <Row className={styles.resultSuccess}>
             <CheckCircleOutlined className={styles.iconSuccess} />
