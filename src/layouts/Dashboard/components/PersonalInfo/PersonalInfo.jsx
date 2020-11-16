eba47095a725564fa4d7dbb3a05aaf916ccfb349
Victor Vorobiov <vv1641627@gmail.com>
2020-11-16 17:23:22 +0200

    Refactor PersonalInfo component

--- src/layouts/Dashboard/components/PersonalInfo/PersonalInfo.jsx
+++ src/layouts/Dashboard/components/PersonalInfo/PersonalInfo.jsx
@@ -3,12 +3,27 @@ import { Collapse, Table } from 'antd';
 
 const { Panel } = Collapse;
 
-const PersonalInfo = ({ columns, data }) => (
-  <Collapse ghost>
-    <Panel header="Personal info" key="2">
-      <Table columns={columns} dataSource={data} pagination={false} />
-    </Panel>
-  </Collapse>
-);
+// eslint-disable-next-line consistent-return
+const PersonalInfo = ({ columns, data }) => {
+  if (!data) {
+    return null;
+  }
+  const { firstName: name, email, lastName } = data;
+  const { timeZone, city } = data.address;
+
+  return (
+    <Collapse ghost>
+      <Panel header="Personal info" key="2">
+        <Table
+          columns={columns}
+          dataSource={[{
+            email, name, lastName, timeZone, city,
+          }]}
+          pagination={false}
+        />
+      </Panel>
+    </Collapse>
+  );
+};
 
 export default PersonalInfo;
