d57ac3e34434d7b81bc360bc1ca246eb85b117cf
Victor Vorobiov <vv1641627@gmail.com>
2020-11-19 16:49:39 +0200

    Add teamMock in index

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,15 +1,17 @@
 import authMocks from './auth';
 import leagueMocks from './league';
 import dashboardMocks from './dashboard';
+import teamMocks from './team';
 
 const mocksCommon = {
-  delay: 6000,
+  delay: 1000,
 };
 
 const mocks = [
   ...authMocks,
   ...dashboardMocks,
   ...leagueMocks,
+  ...teamMocks,
 ].map(
   (m) => ({ ...mocksCommon, ...m }),
 );
