4217c27803ab07b90fc16ec67a37bf3c9a6493dd
Artur Chubukliiev <artur.it2914@gmail.com>
2020-11-23 15:21:57 +0200

    Refactor Logo to empty template

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import LogoIcon from './NICA_lockup.svg';
 import styles from './Logo.module.scss';
 
-const Logo = (props) => (
-  <img src={LogoIcon} alt="NICA" className={styles.logo} {...props} />
+const Logo = () => (
+  <div className={styles.container}>
+    <div className={styles.logo} />
+  </div>
 );
 
 export default Logo;
