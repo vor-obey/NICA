a75ea885085e851fc20fad6ba2ca874a5e3d4949
Victor Vorobiov <vv1641627@gmail.com>
2020-11-27 16:13:46 +0200

    Delete button from League container

--- src/layouts/Dashboard/containers/Leagues/League/League.jsx
+++ src/layouts/Dashboard/containers/Leagues/League/League.jsx
@@ -1,42 +1,17 @@
 import React from 'react';
-import {
-  Row, Popover,
-} from 'antd';
-import {
-  DownloadOutlined,
-} from '@ant-design/icons';
-import styles from './League.module.scss';
+
 import Admins from '../../Admins';
 import LeagueTitle from '../../LeagueTitle';
 import Coaches from '../../Coaches';
 
-const League = () => {
-  const renderButtonCoach = () => (
-    <Row>
-      <Popover
-        placement="left"
-        content={(
-          <div className={styles.popoverLinks}>
-            <a target="_blank" rel="noopener noreferrer" href="/#">Export Coaches CSV</a>
-            <a target="_blank" rel="noopener noreferrer" href="/#">Emergency Contacts CSV</a>
-          </div>
-)}
-        trigger="click"
-      >
-        <DownloadOutlined className={styles.buttonIcons} />
-      </Popover>
-    </Row>
-  );
+const League = () => (
+  <>
+    <LeagueTitle />
 
-  return (
-    <>
-      <LeagueTitle />
+    <Coaches />
 
-      <Coaches buttons={renderButtonCoach || ''} />
-
-      <Admins />
-    </>
-  );
-};
+    <Admins />
+  </>
+);
 
 export default League;
