3f0f34b098d0ff1d8063655fac581f4a968cca65
Victor Vorobiov <vv1641627@gmail.com>
2020-12-01 17:02:48 +0200

    Refactor user profile style

--- src/layouts/Dashboard/containers/User/UserProfile.jsx
+++ src/layouts/Dashboard/containers/User/UserProfile.jsx
@@ -6,9 +6,9 @@ import { gql, useQuery } from '@apollo/client';
 import PageTitle from '../../components/PageTitle';
 import PersonalInformation from './PersonalInformation';
 
-import styles from './Coach.module.scss';
+import styles from './UserProfile.module.scss';
 
-export const DASHBOARD_COACH_QUERY = gql`
+export const USER_QUERY = gql`
     query dashboardCoach($coachId: ID!){
         coach (id: $coachId){
             id
@@ -23,29 +23,23 @@ export const DASHBOARD_COACH_QUERY = gql`
         },
     }`;
 
-const Coach = () => {
-  const { data, loading } = useQuery(DASHBOARD_COACH_QUERY, {
+const UserProfile = () => {
+  const { data, loading } = useQuery(USER_QUERY, {
     variables: {
       coachId: 1,
     },
   });
+
+  const renderTitle = `Coach: ${data?.coach?.name?.firstName} ${data?.coach?.name?.lastName}`;
 
   return (
     <>
       <Row className={styles.logoHeader}>
         <PageTitle
           loading={loading}
-          title={(
-            <div className={styles.titleHeader}>
-              {`Coach: ${data?.coach?.name?.firstName} ${data?.coach?.name?.lastName}`}
-            </div>
-)}
+          title={renderTitle}
           avatar={data?.coach?.league.image}
-          description={(
-            <div className={styles.descriptionHeader}>
-              {`${data?.coach?.league?.name?.short ?? ''} league`}
-            </div>
-          )}
+          description={`${data?.coach?.league?.name?.short ?? ''} league`}
         />
       </Row>
 
@@ -58,4 +52,4 @@ const Coach = () => {
   );
 };
 
-export default Coach;
+export default UserProfile;
