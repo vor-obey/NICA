e874c326a0376c5c34a5e305507f2da3104d5675
Victor Vorobiov <vv1641627@gmail.com>
2020-12-01 17:00:36 +0200

    Refactor index

--- src/layouts/Dashboard/containers/Index/Index.jsx
+++ src/layouts/Dashboard/containers/Index/Index.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import Coach from '../Coach/Coach';
-import Admin from '../Admin';
-import { permissions } from '../../../../configs/app';
-import { USER_ROLE } from '../../../../configs/mock';
-
-const role = USER_ROLE;
+import UserProfile from '../User/UserProfile';
 
 const Index = () => (
-  <>
-    {role === permissions.roles.COACH ? <Coach /> : <Admin /> }
-  </>
+  <UserProfile />
 );
 
 export default Index;
