47eee40b5286b5092d0801f40acb093a7e98c3b1
Artur Chubukliiev <artur.it2914@gmail.com>
2020-12-02 12:06:38 +0200

    Refactor Coaches columns permissions

--- src/layouts/Dashboard/containers/Coaches/Coaches.jsx
+++ src/layouts/Dashboard/containers/Coaches/Coaches.jsx
@@ -3,12 +3,15 @@ import {
   Button, Col,
 } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import { gql, useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import TableWidget from '../../components/TableWidget';
 import { permissions } from '../../../../configs/app';
 import Permissions from '../../../../components/Permissions';
 import useAuthQuery from '../../../../hooks/useAuthQuery';
+import useColumnsPermissionsFilter from '../../../../hooks/useColumnsRolesFilter';
+
+const { roles: ROLES } = permissions;
 
 export const COACH_QUERY = gql`
     query teamData($teamId: ID!){
@@ -27,8 +30,16 @@ export const COACH_QUERY = gql`
         },
     }`;
 
-const columns = {
-  [permissions.roles.COACH]: [{
+const tableColumns = [
+  {
+    key: 'league',
+    title: 'Leagues',
+    ellipsis: true,
+    dataIndex: 'league',
+    render: (text, record) => <Link to={`/leagues/${record.id}`}>{text}</Link>,
+    roles: [ROLES.SUPER_ADMIN],
+  },
+  {
     key: 'name',
     title: 'UserProfile Name',
     ellipsis: true,
@@ -61,95 +72,16 @@ const columns = {
     dataIndex: 'hours',
     width: 70,
   },
-  ],
-  [permissions.roles.SUPER_ADMIN]: [
-    {
-      key: 'league',
-      title: 'Leagues',
-      ellipsis: true,
-      dataIndex: 'league',
-      render: (text, record) => <Link to={`/leagues/${record.id}`}>{text}</Link>,
-    },
-    {
-      key: 'name',
-      title: 'UserProfile Name',
-      ellipsis: true,
-      dataIndex: 'name',
-      render: (text, record) => <Link to={`/coaches/${record.id}`}>{text}</Link>,
-    },
-    {
-      ellipsis: true,
-      key: 'level',
-      title: 'Level',
-      dataIndex: 'level',
-      align: 'center',
-    },
-    {
-      ellipsis: true,
-      key: 'email',
-      title: 'Email',
-      dataIndex: 'email',
-    },
-    {
-      ellipsis: true,
-      key: 'phone',
-      title: 'Phone',
-      dataIndex: 'phone',
-    },
-    {
-      ellipsis: true,
-      key: 'TTCHours',
-      title: 'TTC Hours',
-      dataIndex: 'hours',
-      align: 'center',
-      width: 70,
-    },
-  ],
-  [permissions.roles.LEAGUE_ADMIN]: [{
-    key: 'name',
-    title: 'UserProfile Name',
-    ellipsis: true,
-    dataIndex: 'name',
-    render: (text, record) => <Link to={`/coaches/${record.id}`}>{text}</Link>,
-  },
-  {
-    ellipsis: true,
-    key: 'level',
-    title: 'Level',
-    dataIndex: 'level',
-    align: 'center',
-  },
-  {
-    ellipsis: true,
-    key: 'email',
-    title: 'Email',
-    dataIndex: 'email',
-  },
-  {
-    ellipsis: true,
-    key: 'phone',
-    title: 'Phone',
-    dataIndex: 'phone',
-  },
-  {
-    ellipsis: true,
-    key: 'TTCHours',
-    title: 'TTC Hours',
-    dataIndex: 'hours',
-    width: 70,
-    align: 'center',
-  },
-  ],
-};
+];
 
-const Coaches = () => {
+const Coaches = ({ columns }) => {
   const { loading, data } = useAuthQuery(COACH_QUERY, {
     variables: {
       teamId: 1,
     },
   });
 
-  const role = data?.coaches?.role;
+  const filteredColumns = useColumnsPermissionsFilter(columns);
 
   const inviteCoaches = () => (
     <Permissions roles={[permissions.roles.LEAGUE_ADMIN, permissions.roles.SUPER_ADMIN]}>
@@ -167,7 +99,7 @@ const Coaches = () => {
       <TableWidget
         rowKey="id"
         pagination={{ pageSize: 15 }}
-        columns={columns[role] || []}
+        columns={filteredColumns}
         loading={loading}
         title="Coaches"
         dataSource={loading ? null : data?.coaches?.coach}
@@ -175,6 +107,10 @@ const Coaches = () => {
       />
     </Col>
   );
+};
+
+Coaches.defaultProps = {
+  columns: tableColumns,
 };
 
 export default Coaches;
