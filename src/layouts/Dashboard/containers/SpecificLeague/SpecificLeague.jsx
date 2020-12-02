5835228202760dbc79941961a91f3085d5bf9aae
Victor Vorobiov <vv1641627@gmail.com>
2020-12-02 15:37:58 +0200

    Refactor coaches logic

--- src/layouts/Dashboard/containers/SpecificLeague/SpecificLeague.jsx
+++ src/layouts/Dashboard/containers/SpecificLeague/SpecificLeague.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Coaches from '../Coaches';
 import LeagueTitle from '../LeagueTitle';
 import LeagueAdmins from './components/LeagueAdmins';
@@ -7,10 +8,46 @@ import { permissions } from '../../../../configs/app';
 
 const { roles: ROLES } = permissions;
 
+const tableColumns = [
+  {
+    key: 'name',
+    title: 'UserProfile Name',
+    ellipsis: true,
+    dataIndex: 'name',
+    render: (text, record) => <Link to={`/coaches/${record.id}`}>{text}</Link>,
+  },
+  {
+    ellipsis: true,
+    key: 'level',
+    title: 'Level',
+    dataIndex: 'level',
+    align: 'center',
+  },
+  {
+    ellipsis: true,
+    key: 'email',
+    title: 'Email',
+    dataIndex: 'email',
+  },
+  {
+    ellipsis: true,
+    key: 'phone',
+    title: 'Phone',
+    dataIndex: 'phone',
+  },
+  {
+    ellipsis: true,
+    key: 'TTCHours',
+    title: 'TTC Hours',
+    dataIndex: 'hours',
+    width: 70,
+  },
+];
+
 const SpecificLeague = () => (
   <>
     <LeagueTitle />
-    <Coaches />
+    <Coaches columns={tableColumns} />
     <Permissions roles={[ROLES.SUPER_ADMIN, ROLES.LEAGUE_ADMIN]}>
       <LeagueAdmins />
     </Permissions>
