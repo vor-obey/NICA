05843edab54acea5f6d5a001eafc7fdcc4cc851c
Victor Vorobiov <vv1641627@gmail.com>
2020-12-03 10:26:10 +0200

    Delete storybook stories

--- src/stories/LoginForm/LoginForm.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LoginForm from '../../screens/Auth/components/LoginForm';
-
-export default {
-  title: 'LoginForm',
-  component: LoginForm,
-};
-
-export const Template = () => <LoginForm />;
--- src/stories/SignUpForm/SignUpForm.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SignUpForm from '../../screens/Auth/components/SignUpForm';
-
-export default {
-  title: 'SignUpForm',
-  components: SignUpForm,
-};
-
-export const Template = () => <SignUpForm onSubmit={() => {}} />;
