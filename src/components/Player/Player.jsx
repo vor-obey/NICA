1d8a8fe97a772aaac54f17ea669dd1eb58664064
Victor Vorobiov <vv1641627@gmail.com>
2020-12-09 13:05:02 +0200

    Add button play/stop

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,27 +1,43 @@
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
+import { CaretRightOutlined, PauseOutlined } from '@ant-design/icons';
+import styles from './Player.module.scss';
 
 const Player = ({ onStart, onFinish, url }) => {
   const [durationVideo, setDurationVideo] = useState(0);
   const [ended, setEnded] = useState(false);
+  const [play, setPlay] = useState(false);
+
+  const onPlaying = () => (
+    setPlay(!play)
+  );
+
+  const renderButton = (
+    play
+      ? <PauseOutlined onClick={onPlaying} className={styles.playBtn} />
+      : <CaretRightOutlined onClick={onPlaying} className={styles.playBtn} />
+  );
 
   return (
-    <ReactPlayer
-      width="70%"
-      height="60%"
-      controls
-      url={url}
-      onStart={() => onStart()}
-      onEnded={() => onFinish()}
-      onProgress={(state) => {
-        const percent = durationVideo / 10;
-        if (state.playedSeconds >= durationVideo - percent && !ended) {
-          setEnded(true);
-          onFinish();
-        }
-      }}
-      onDuration={(duration) => setDurationVideo(duration)}
-    />
+    <div className={styles.playerWrapper}>
+      {renderButton}
+      <ReactPlayer
+        playing={play}
+        width="100%"
+        height="60%"
+        url={url}
+        onStart={() => onStart()}
+        onEnded={() => onFinish()}
+        onProgress={(state) => {
+          const percent = durationVideo / 10;
+          if (state.playedSeconds >= durationVideo - percent && !ended) {
+            setEnded(true);
+            onFinish();
+          }
+        }}
+        onDuration={(duration) => setDurationVideo(duration)}
+      />
+    </div>
   );
 };
 
