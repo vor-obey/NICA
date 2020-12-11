15de42088c54250984ec3425b22d60dccec35171
Victor Vorobiov <vv1641627@gmail.com>
2020-12-11 11:03:17 +0200

    Wrap skeleton

--- src/components/LicenseProgress/LicenseProgress.jsx
+++ src/components/LicenseProgress/LicenseProgress.jsx
@@ -1,24 +1,26 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Skeleton } from 'antd';
 import PropTypes from 'prop-types';
 import ProgressSteps from './ProgressSteps';
 
 const LicenseProgress = ({
-  steps, currentStepIndex, children, setCurrentStep,
+  steps = [], currentStepIndex, children, setCurrentStep, loading,
 }) => (
-  <Row gutter={40}>
-    <Col span={4}>
-      <ProgressSteps
-        steps={steps}
-        onChange={setCurrentStep}
-        current={currentStepIndex}
-      />
-    </Col>
-    <Col span={20}>
-      {children}
-    </Col>
-  </Row>
+  <Skeleton loading={loading}>
+    <Row gutter={40}>
+      <Col span={4}>
+        <ProgressSteps
+          steps={steps}
+          onChange={setCurrentStep}
+          current={currentStepIndex}
+        />
+      </Col>
+      <Col span={20}>
+        {children}
+      </Col>
+    </Row>
+  </Skeleton>
 );
 
 export const StepPropType = PropTypes.shape({
