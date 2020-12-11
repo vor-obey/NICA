87286826c63dea2ae99cecac77d2412a80fa2b76
Victor Vorobiov <vv1641627@gmail.com>
2020-12-11 11:04:46 +0200

    Refactor licenseSteps

--- src/layouts/Dashboard/containers/LicenseSteps/LicenseSteps.jsx
+++ src/layouts/Dashboard/containers/LicenseSteps/LicenseSteps.jsx
@@ -1,82 +1,29 @@
 import React, { useCallback, useEffect, useMemo } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { gql } from '@apollo/client';
 import LicenseProgress from '../../../../components/LicenseProgress';
-import QUESTION_TYPE from '../../../../utils/constants';
 import LicenseStepContainer from '../../components/LicenseStepContainer';
+import useAuthQuery from '../../../../hooks/useAuthQuery';
 
-const steps = [
-  {
-    id: 1,
-    title: 'Introduction video',
-    description: 'See the video to understand our goals',
-    type: QUESTION_TYPE.VIDEO,
-    data: {
-      url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-    },
-  },
-  {
-    id: 2,
-    title: 'Upload first aid Certificate',
-    description: 'Document upload',
-    type: QUESTION_TYPE.FILE_UPLOAD,
-    data: {},
-  },
-  {
-    id: 3,
-    title: 'Coach agreement',
-    description: 'Read and confirm the document',
-    type: QUESTION_TYPE.AGREEMENT,
-    data: {
-      document: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
-        + 'Ad, cum dolorem ducimus impedit incidunt quasi quod totam? '
-        + 'Aspernatur atque, autem consectetur dolore eos et exercitationem, '
-        + 'expedita ipsum iste minima nisi odit omnis quidem sint tempore velit '
-        + 'veritatis voluptatem voluptatibus! Alias autem doloribus explicabo impedit '
-        + 'labore minima quae quidem, quo quos rem repellendus reprehenderit '
-        + 'soluta voluptatibus. Aut et eveniet facilis laudantium repellendus.'
-        + ' Adipisci aliquam cupiditate explicabo fugiat in iusto non, quos '
-        + 'reiciendis, reprehenderit similique tenetur voluptate? Aspernatur '
-        + 'assumenda consequatur cupiditate dicta, distinctio ducimus error'
-        + ' explicabo ipsam iusto laudantium maxime nam nobis quam quasi quisquam'
-        + ' reprehenderit repudiandae soluta tempora tenetur velit veniam veritatis '
-        + 'voluptas voluptates! Asperiores, eum exercitationem explicabo ipsa iste libero '
-        + 'magni molestias nam nesciunt porro qui, quisquam sint tenetur veniam'
-        + ' veritatis. Dicta hic ipsa itaque magnam nihil non obcaecati officiis,'
-        + ' porro sapiente soluta, veniam voluptates! Asperiores consectetur cum'
-        + ' cumque dignissimos eaque eos error excepturi facilis harum incidunt '
-        + 'itaque natus nesciunt nihil nisi non odio, officiis omnis, perspiciatis '
-        + 'quae quod tempora tempore tenetur ut veritatis voluptas! Enim error'
-        + ' esse iusto nesciunt sapiente? Dolor dolore facilis, fugiat, laborum molestias, '
-        + 'nobis non optio perspiciatis porro quisquam quos vitae? Aut error'
-        + ' ex facere mollitia nulla odio optio perferendis sint? Alias eos maxime'
-        + ' praesentium totam!'
-        + 'Ad, cum dolorem ducimus impedit incidunt quasi quod totam? '
-        + 'Aspernatur atque, autem consectetur dolore eos et exercitationem, '
-        + 'expedita ipsum iste minima nisi odit omnis quidem sint tempore velit '
-        + 'veritatis voluptatem voluptatibus! Alias autem doloribus explicabo impedit '
-        + 'labore minima quae quidem, quo quos rem repellendus reprehenderit '
-        + 'soluta voluptatibus. Aut et eveniet facilis laudantium repellendus.'
-        + ' Adipisci aliquam cupiditate explicabo fugiat in iusto non, quos '
-        + 'reiciendis, reprehenderit similique tenetur voluptate? Aspernatur '
-        + 'assumenda consequatur cupiditate dicta, distinctio ducimus error'
-        + ' explicabo ipsam iusto laudantium maxime nam nobis quam quasi quisquam'
-        + ' reprehenderit repudiandae soluta tempora tenetur velit veniam veritatis '
-        + 'voluptas voluptates! Asperiores, eum exercitationem explicabo ipsa iste libero '
-        + 'magni molestias nam nesciunt porro qui, quisquam sint tenetur veniam'
-        + ' veritatis. Dicta hic ipsa itaque magnam nihil non obcaecati officiis,'
-        + ' porro sapiente soluta, veniam voluptates! Asperiores consectetur cum'
-        + ' cumque dignissimos eaque eos error excepturi facilis harum incidunt '
-        + 'itaque natus nesciunt nihil nisi non odio, officiis omnis, perspiciatis '
-        + 'quae quod tempora tempore tenetur ut veritatis voluptas! Enim error'
-        + ' esse iusto nesciunt sapiente? Dolor dolore facilis, fugiat, laborum molestias, '
-        + 'nobis non optio perspiciatis porro quisquam quos vitae? Aut error'
-        + ' ex facere mollitia nulla odio optio perferendis sint? Alias eos maxime'
-        + ' praesentium totam!',
-    },
-  },
-];
+export const LICENSE_STEPS_QUERY = gql`
+    query licenseData($licenseId: ID!){
+        steps(id: $licenseId) {
+            id,
+            title,
+            description,
+            type,
+            data,
+            quiz,
+        },
+    }`;
 
 const LicenseSteps = () => {
+  const { loading, data } = useAuthQuery(LICENSE_STEPS_QUERY, {
+    variables: {
+      licenseId: 1,
+    },
+  });
+
   const history = useHistory();
   const { licenseId, index } = useParams();
 
@@ -87,7 +34,7 @@ const LicenseSteps = () => {
   }, []);
 
   const setCurrentStep = useCallback((nextStep) => {
-    if (!steps[nextStep]) return;
+    if (data?.steps && !data?.steps[nextStep]) return;
 
     history.push(`/licenses/${licenseId}/step/${nextStep}`);
   }, [history]);
@@ -97,8 +44,13 @@ const LicenseSteps = () => {
   }, [setCurrentStep, currentIndex]);
 
   return (
-    <LicenseProgress steps={steps} setCurrentStep={setCurrentStep} currentStepIndex={currentIndex}>
-      <LicenseStepContainer step={steps[currentIndex]} goNext={goNext} />
+    <LicenseProgress
+      loading={loading}
+      steps={data?.steps}
+      setCurrentStep={setCurrentStep}
+      currentStepIndex={currentIndex}
+    >
+      <LicenseStepContainer loading={loading} step={data?.steps[currentIndex]} goNext={goNext} />
     </LicenseProgress>
   );
 };
